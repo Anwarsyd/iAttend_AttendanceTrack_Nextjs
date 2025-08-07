3c1be5de0170aecef9f3f23c5191172852e2df20
anwar <anwarnsyd@gmail.com>
2025-08-07 08:29:23 +0530

    Attendance Schema Added

---
+++ app/dashboard/Attendance/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Attendance() {
+  return (
+    <div>
+      <h1>Attendance Page</h1>
+    </div>
+  )
+}
+
+export default Attendance
--- utils/schema.js
+++ utils/schema.js
@@ -1,4 +1,4 @@
-import { mysqlTable,int, varchar } from "drizzle-orm/mysql-core";
+import { mysqlTable,int, varchar,boolean } from "drizzle-orm/mysql-core";
 import { Contact, Phone } from "lucide-react";
 
 export const GRADES = mysqlTable('grades',{
@@ -12,4 +12,12 @@ export const STUDENTS = mysqlTable('students',{
     grade:varchar('grade',{length:10}).notNull(),
     address:varchar('address',{length:50}),
     contact:varchar('contact',{length:11})
+})
+
+export const ATTENDANCE=mysqlTable('attendance',{
+    id:int('id',{length:11}).autoincrement().primaryKey(),
+    studentId:int('studentId',{length:11}).notNull(),
+    present: boolean('present').default(false),
+    day:int('day',{length:11}).notNull(),  //28
+    date:varchar('date',{length:20}).notNull() //07/2024
 })
